dd9905193ee025a058364c3489b9336417177527
FelipeSouza14 <felipe.dev.2003@gmail.com>
2025-06-07 14:40:42 -0300

    feat(title): implementar o ciclo de vida OnChange

--- mod-4-life-cycle-hooks-angular/src/app/title/title.component.ts
+++ mod-4-life-cycle-hooks-angular/src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,15 +6,20 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
   templateUrl: './title.component.html',
   styleUrl: './title.component.css'
 })
-export class TitleComponent implements OnInit {
-    nome:string = 'Felipe'
+export class TitleComponent implements OnInit, OnChanges {
+
+    @Input() nome:string = ''
 
     constructor() {
         console.log(`Constructor ${this.nome}`)
     }
 
+    ngOnChanges(): void {
+        console.log(`OnChange ${this.nome}`)
+    }
+
     ngOnInit(): void {
-        this.nome = `Ola ${this.nome}`
+        console.log(`OnInit ${this.nome}`)
     }
 
 }
